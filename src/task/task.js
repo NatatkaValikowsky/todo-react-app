83cccb170ebf36b5dfe32dabc6845a8c5f45a8f7
Valikova Natalya <valikowa.natalya@yandex.ru>
2020-12-06 22:53:50 +0900

    set task on hooks

--- src/task/task.js
+++ src/task/task.js
@@ -1,141 +1,127 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { formatDistanceToNow } from 'date-fns';
 import './task.css';
 import PropTypes from 'prop-types';
 import Timer from '../timer';
 
-const initialState = {
-	title: ''
-};
+const Task = (props) => {
 
-export default class Task extends Component {
+	const {
+		id,
+		title,
+		date,
+		isCompleted,
+		isEditing,
+		onSetCompleted,
+		onDeleted,
+		saveNewTitle,
+		closeEditField,
+		onStartTimer,
+		isCurrentTimer,
+		onEdited
+	} = props;
 
-	state = {...initialState};
+	const [_title, setTitle ] = useState(title);
+	const [_editedTitle, setEditedTitle ] = useState(title);
 
-	componentDidMount() {
-		this.setState(this.props)
-	}
+	const classes = (isCompleted ? ' completed' : '') + (isEditing ? ' editing' : '');
 
-	onChangeTitle = (event) => {
-		this.setState({
-			editedTitle: event.target.value,
-		});
-	};
-
-	onClickEditButton = (func) => {
-		const { title } = this.state;
-		this.setState({
-			editedTitle: title,
-		});
-
-		func();
-		document.addEventListener('keydown', this.escKeyHandler);
-	};
-
-	escKeyHandler = (event) => {
+	const escKeyHandler = (event) => {
 		if (event.keyCode === 27) {
-			const { closeEditField, id } = this.state;
 			closeEditField(id);
 		}
 	};
 
-	render() {
-		const {
-			id,
-			date,
-			isCompleted,
-			isEditing,
-			onSetCompleted,
-			onEdited,
-			onDeleted,
-			saveNewTitle,
-			closeEditField,
-			onStartTimer,
-			isCurrentTimer,
-		} = this.props;
-		const { title, editedTitle } = this.state;
-
-		const classes = (isCompleted ? ' completed' : '') + (isEditing ? ' editing' : '');
-
-		if (!isEditing) {
-			document.removeEventListener('keydown', this.escKeyHandler);
-		}
-
-
-		return (
-			<li key={id} className={classes}>
-				<div className="view">
-					<input
-						id={`list-element-${id}`}
-						className="toggle"
-						type="checkbox"
-						onChange={onSetCompleted}
-						checked={isCompleted}
-					/>
-					<label htmlFor={`list-element-${id}`}>
-						<span className="description">{title}</span>
-						<Timer
-							onStartTimer={onStartTimer}
-							elId={id}
-							isCurrentTimer={isCurrentTimer && !isCompleted && !isEditing}
-							couldStart={!isCompleted}
-						/>
-						<span className="created">created {formatDistanceToNow(date, { addSuffix: true })}</span>
-					</label>
-					{!isCompleted ? (
-						<button
-							type="button"
-							className="icon icon-edit"
-							onClick={() => {
-								this.onClickEditButton(onEdited);
-							}}
-							aria-label="Edit button"
-						/>
-					) : null}
-					<button type="button" className="icon icon-destroy" onClick={onDeleted} aria-label="Delete button" />
-				</div>
-
-				{isEditing ? (
-					<form
-						onSubmit={(event) => {
-							event.preventDefault();
-							this.setState({
-								title: editedTitle,
-							});
-							saveNewTitle(id, title);
-							closeEditField(id);
-						}}
-					>
-						<input type="text" className="edit" value={editedTitle || title} onChange={this.onChangeTitle} />
-					</form>
-				) : null}
-			</li>
-		);
+	if (!isEditing) {
+		document.removeEventListener('keydown', escKeyHandler);
 	}
-}
+
+	const onClickEditButton = (func) => {
+		setEditedTitle(_title);
+
+		func();
+		document.addEventListener('keydown', escKeyHandler);
+	};
+
+	const onChangeTitle = (event) => {
+		setEditedTitle( event.target.value);
+	};
+
+	return(
+		<li key={id} className={classes}>
+			<div className="view">
+				<input
+					id={`list-element-${id}`}
+					className="toggle"
+					type="checkbox"
+					onChange={onSetCompleted}
+					checked={isCompleted}
+				/>
+				<label htmlFor={`list-element-${id}`}>
+					<span className="description">{_title}</span>
+					<Timer
+						onStartTimer={onStartTimer}
+						elId={id}
+						isCurrentTimer={isCurrentTimer && !isCompleted && !isEditing}
+						couldStart={!isCompleted}
+					/>
+					<span className="created">created {formatDistanceToNow(date, { addSuffix: true })}</span>
+				</label>
+				{!isCompleted ? (
+					<button
+						type="button"
+						className="icon icon-edit"
+						onClick={() => {
+							setTitle(_title);
+							onClickEditButton(onEdited);
+						}}
+						aria-label="Edit button"
+					/>
+				) : null}
+				<button type="button" className="icon icon-destroy" onClick={onDeleted} aria-label="Delete button" />
+			</div>
+
+			{isEditing ? (
+				<form
+					onSubmit={(event) => {
+						event.preventDefault();
+						setTitle(_editedTitle);
+						saveNewTitle(id, _title);
+						closeEditField(id);
+					}}
+				>
+					<input type="text" className="edit" value={_editedTitle || _title} onChange={onChangeTitle} />
+				</form>
+			) : null}
+		</li>
+	);
+};
+
+export default Task;
 
 Task.defaultProps = {
 	isCompleted: false,
 	isEditing: false,
 	onSetCompleted: () => {},
-	onEdited: () => {},
 	onDeleted: () => {},
 	saveNewTitle: () => {},
 	closeEditField: () => {},
 	onStartTimer: () => {},
 	isCurrentTimer: false,
+	onEdited: () => {}
 };
 
 Task.propTypes = {
 	id: PropTypes.number.isRequired,
+	title: PropTypes.string.isRequired,
 	date: PropTypes.instanceOf(Date).isRequired,
 	isCompleted: PropTypes.bool,
 	isEditing: PropTypes.bool,
 	onSetCompleted: PropTypes.func,
-	onEdited: PropTypes.func,
 	onDeleted: PropTypes.func,
 	saveNewTitle: PropTypes.func,
 	closeEditField: PropTypes.func,
 	onStartTimer: PropTypes.func,
 	isCurrentTimer: PropTypes.bool,
+	onEdited: PropTypes.func
 };
