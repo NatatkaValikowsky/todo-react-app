ed8e5c4e9417e7f9fd44646efba992943b30272f
Valikova Natalya <valikowa.natalya@yandex.ru>
2020-12-06 23:07:23 +0900

    set TaskList to hooks

--- src/task-list/task-list.js
+++ src/task-list/task-list.js
@@ -1,51 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Task from '../task';
 import './task-list.css';
 
-export default class TaskList extends Component {
-	state = {
-		startedTimerId: null,
+const TaskList = (props) => {
+
+	const [ startedTimerId, setStartedTimerId] = useState(null);
+
+	const setCurrentTimerId = (id) => {
+		setStartedTimerId(id);
 	};
 
-	setCurrentTimerId = (id) => {
-		this.setState({
-			startedTimerId: id,
+	const { items, filterType, onDeleted, onSetCompleted, onEdited, saveNewTitle, closeEditField } = props;
+
+	const elements = items
+		.filter((el) => {
+			return (
+				filterType === 'all' ||
+				(filterType === 'active' && !el.isCompleted) ||
+				(filterType === 'completed' && el.isCompleted)
+			);
+		})
+		.map((item) => {
+			return (
+				<Task
+					key={item.id}
+					{...item}
+					onDeleted={() => onDeleted(item.id)}
+					onSetCompleted={() => onSetCompleted(item.id)}
+					onEdited={() => onEdited(item.id)}
+					saveNewTitle={saveNewTitle}
+					closeEditField={closeEditField}
+					isCurrentTimer={startedTimerId === item.id}
+					onStartTimer={setCurrentTimerId}
+				/>
+			);
 		});
-	};
 
-	render() {
-		const { startedTimerId } = this.state;
+	return <ul className="todo-list">{elements}</ul>;
+};
 
-		const { items, filterType, onDeleted, onSetCompleted, onEdited, saveNewTitle, closeEditField } = this.props;
-
-		const elements = items
-			.filter((el) => {
-				return (
-					filterType === 'all' ||
-					(filterType === 'active' && !el.isCompleted) ||
-					(filterType === 'completed' && el.isCompleted)
-				);
-			})
-			.map((item) => {
-				return (
-					<Task
-						key={item.id}
-						{...item}
-						onDeleted={() => onDeleted(item.id)}
-						onSetCompleted={() => onSetCompleted(item.id)}
-						onEdited={() => onEdited(item.id)}
-						saveNewTitle={saveNewTitle}
-						closeEditField={closeEditField}
-						isCurrentTimer={startedTimerId === item.id}
-						onStartTimer={this.setCurrentTimerId}
-					/>
-				);
-			});
-
-		return <ul className="todo-list">{elements}</ul>;
-	}
-}
+export default TaskList;
 
 TaskList.defaultProps = {
 	onDeleted: () => {},
